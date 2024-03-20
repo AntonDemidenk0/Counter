17806b269b54f69070fc854b8413b398468d4be2
Anton Demidenko <putiatish@gmail.com>
2024-03-20 14:34:03 +0100

    Выполнена усложненная задача

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -8,36 +8,52 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    @IBOutlet weak var value: UILabel!
-    @IBOutlet weak var countButton: UIButton!
     
+    @IBOutlet weak var value: UILabel!
+    @IBOutlet weak var plusButton: UIButton!
+    @IBOutlet weak var minusButton: UIButton!
+    @IBOutlet weak var trashButton: UIButton!
+    @IBOutlet weak var textField: UITextView!
+    var numberOfTaps = 0
     override func viewDidLoad() {
         super.viewDidLoad()
+        textField.isEditable = false
+        textField.isScrollEnabled = true
+        textField.text = "История изменений:\n"
+        updateScreen()
         // Do any additional setup after loading the view.
     }
-    var numberOfTaps = 0
-    @IBAction func buttonDidTap(_ sender: Any) {
-        numberOfTaps += 1
-        var text = "Значение счётчика: \(numberOfTaps)"
-        if text.count > 18 {
-                // Разделение текста на строки
-                let startIndex = text.index(text.startIndex, offsetBy: 18)
-                let secondLine = text[startIndex...]
-                text = "\(text.prefix(18))\n\(secondLine)"
-                
-                // Установка размера шрифта 72 для оставшейся части текста
-                let range = NSRange(location: 18, length: text.count - 18)
-                let attributedString = NSMutableAttributedString(string: text)
-                attributedString.addAttribute(.font, value: UIFont.systemFont(ofSize: 72), range: range)
-                
-                // Устанавливаем атрибутированную строку в UILabel
-                value.attributedText = attributedString
+    @IBAction func addButtonPressed(_ sender: UIButton) {
+        let dateFormatter = DateFormatter()
+            dateFormatter.dateFormat = "[dd-MM-yy HH:mm:ss]"
+        let dateString = dateFormatter.string(from: Date())
+//plusButton
+        if sender.tag == 1 {
+            numberOfTaps += 1
+            textField.text += "\(dateString): значение изменено на +1\n"
+            updateScreen()
+        }
+//minusButton
+        if sender.tag == 2 {
+            numberOfTaps -= 1
+            if numberOfTaps < 0 {
+                textField.text += "\(dateString): попытка уменьшить значение счётчика ниже 0\n"
+                numberOfTaps = 0
             } else {
-                // Если длина текста не превышает 18 символов, устанавливаем его без изменений
-                value.text = text
+                textField.text += "\(dateString): значение изменено на -1\n"
+            }
+            updateScreen()
+        }
+//trashButton
+        if sender.tag == 3 {
+            numberOfTaps = 0
+            textField.text += "\(dateString): значение счётчика сброшено\n"
+            updateScreen()
             }
         }
+        func updateScreen() {
+            value.text = "Значение счётчика: \(numberOfTaps)"
+            let range = NSMakeRange(textField.text.count - 1, 0)
+            textField.scrollRangeToVisible(range)
         }
-    
-
+    }
